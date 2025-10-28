1adad093300e3bb172d20860217e1ed9ec7069fd
Shrushti Patil <shrushtipatil0508@gmail.com>
2025-10-28 14:09:59 +0530

    Backend ready for deployment

--- backend/config/mongoose-connection.js
+++ backend/config/mongoose-connection.js
@@ -1,12 +1,11 @@
-require('dotenv').config();
+require('dotenv').config(); // load .env variables
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log("Database Connection Done!");
-  })
-  .catch((err) => {
-    console.error("MongoDB connection error:", err);
-  });
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("Database Connection Done!"))
+.catch((err) => console.error("MongoDB connection error:", err));
 
 module.exports = mongoose.connection;
