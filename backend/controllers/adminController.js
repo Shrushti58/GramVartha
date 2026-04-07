d1ef7d097c1d5ac52597c025a12a85e1a105d7cd
Shrushti Patil <shrushtipatil0508@gmail.com>
2026-04-07 20:58:26 +0530

    Added Marathi support

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -60,12 +60,12 @@ const loginAdmin = async (req, res) => {
         }
 
         const token = generateToken(admin);
-        res.cookie("token", token, {
-            httpOnly: true,
-            secure: false,       
-            sameSite: "lax",     
-            maxAge: 24 * 60 * 60 * 1000
-        });
+       res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,          
+  sameSite: "none",      
+  maxAge: 24 * 60 * 60 * 1000
+});
 
         return res.status(200).json({ message: "Login successful" });
     } catch (error) {
